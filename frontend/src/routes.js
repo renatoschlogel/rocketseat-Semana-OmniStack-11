ce1c7a0731abff313cdce09815fdcd8b9d0d7d54
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-04-02 18:40:19 -0300

    conslusão da estilização das paginas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/logon/Logon';
 import Register from './pages/register/Register';
+import Profile from './pages/profile/Profile';
+import NewIncident from './pages/newincident/NewIncident';
 
 export default function Routes(){
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon}/>
-        <Route path="/register" component={Register}/>
+        <Route path="/register" exact component={Register}/>
+        <Route path="/profile" exact component={Profile}/>
+        <Route path="/incidents/new" exact component={NewIncident}/>
       </Switch>
     </BrowserRouter>
   );
